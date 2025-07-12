1367dba995a0b9598b3688e47fdaf2f7ca320e15
Harsh Yadav <yharsh579@gmail.com>
2025-07-12 17:26:59 +0530

    hh

--- cricketgameproject/index.js
+++ cricketgameproject/index.js
@@ -1,15 +1,29 @@
-// compter choose
-// let randomvalue=Math.random()*3;
-// if(randomvalue>0&& randomvalue<=1){
-//     let computerchoice='bat';
-//     console.log("computer choice is bat");
+// // compter choose
+// // let randomvalue=Math.random()*3;
+// // if(randomvalue>0&& randomvalue<=1){
+// //     let computerchoice='bat';
+// //     console.log("computer choice is bat");
 
-// }
-// else if(randomNumber>1 && randomNumber<=2){
-//     let computerchoice='ball';
-//     console.log("computer choice is ball");
-// }
-// else{
-//     let computerchoice='stump';
-//     console.log("computer choice is stump");
+// // }
+// // else if(randomNumber>1 && randomNumber<=2){
+// //     let computerchoice='ball';
+// //     console.log("computer choice is ball");
+// // }
+// // else{
+// //     let computerchoice='stump';
+// //     console.log("computer choice is stump");
+// // }
+
+// let computerChoice;
+
+// function generatecomputerchoice(){
+//     let randomValue = Math.random() * 3;
+    
+//     if (randomValue > 0 && randomValue <= 1) {
+//       computerChoice = 'bat';
+//     } else if (randomValue > 1 && randomValue <= 2) {
+//       computerChoice = 'ball';
+//     } else {
+//       computerChoice = 'stump';
+//     }
 // }
